628bccce654c1409a25b0d3fd8e475b1537a8717
MoHaNaD-mjs <s1a@hotmail.no>
2022-08-02 21:03:30 +0300

    age-calculator-method

--- src/main/java/com/example/model/Person.java
+++ src/main/java/com/example/model/Person.java
@@ -4,10 +4,13 @@ public class Person {
 	//Test comment added
 	//add comment 2
 	//test adding email notification 3
+	// age calculator (Date of Birth - current date)
 	String id;
 	String firstName;
 	String lastName;
 	int    age;
+	int DateofBirth;
+	int CurrentDate;
 	public String getId() {
 		return id;
 	}
@@ -31,6 +34,11 @@ public class Person {
 	}
 	public void setAge(int age) {
 		this.age = age;
+	}
+	public void AgeCalculator(int DateofBirth ,int CurrentDate, int age)
+	{
+		age=(DateofBirth-CurrentDate);
+		
 	}
 
 }
